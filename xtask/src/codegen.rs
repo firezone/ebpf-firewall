8ca448d455009289205d5b57dbe9a3b45154dc45
conectado <gabriel@firezone.dev>
2022-08-31 21:16:39 -0300

    fix aya-tool naming

--- xtask/src/codegen.rs
+++ xtask/src/codegen.rs
@@ -1,11 +1,11 @@
-use aya_gen::generate::InputFile;
+use aya_tool::generate::InputFile;
 use std::{fs::File, io::Write, path::PathBuf};
 
 pub fn generate() -> Result<(), anyhow::Error> {
     // TODO: Here there must be a better way to get the app directory
     let dir = PathBuf::from("ebpf-firewall-ebpf/src");
     let names: Vec<&str> = vec!["iphdr", "ipv6hdr", "ethhdr", "udphdr", "tcphdr"];
-    let bindings = aya_gen::generate(
+    let bindings = aya_tool::generate(
         InputFile::Btf(PathBuf::from("/sys/kernel/btf/vmlinux")),
         &names,
         &[],
