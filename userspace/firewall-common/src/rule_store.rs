c8a7006ea0048000fda1d3f2ddef222b673fc1db
conectado <gabriel@firezone.dev>
2022-09-09 22:51:48 -0300

    isolate lookup

--- userspace/firewall-common/src/rule_store.rs
+++ userspace/firewall-common/src/rule_store.rs
@@ -1,3 +1,4 @@
+mod lookup;
 mod test;
 mod user;
 
@@ -5,8 +6,6 @@ mod user;
 pub use user::RuleStoreError;
 
 pub const MAX_RULES: usize = 1024;
-#[cfg(not(feature = "user"))]
-const MAX_ITER: u32 = MAX_RULES.ilog2() + 1;
 // 0xFF should be reserved so this should work forever....
 // We have some free bytes in RuleStore we could as well use a u16 and 0x0100
 pub const GENERIC_PROTO: u8 = 0xFF;
@@ -56,76 +55,6 @@ pub struct RuleStore {
     /// Keep this to < usize::MAX pretty please
     /// But we do need the padding
     rules_len: u32,
-}
-
-impl RuleStore {
-    // TODO: We need to check if this works in older kernels.
-    // If it doesn't we need to use --unroll-loop.
-    // We might need to refactor the loop to explicitly use `MAX_ITER`
-    // Note: MAX_ITER is at most 16
-    pub fn lookup(&self, val: u16) -> bool {
-        // 0 means all ports
-        if self.rules_len > 0 {
-            // SAFETY: We know that rules_len < MAX_RULES
-            if start(*unsafe { self.rules.get_unchecked(0) }) == 0 {
-                return true;
-            }
-        }
-
-        // We test for 0 in all non tcp/udp packets
-        // it's worth returning early for those cases.
-        if val == 0 {
-            return false;
-        }
-
-        // Reimplementation of partition_point to satisfy verifier
-        let mut size = self.rules_len as usize;
-        // appeasing the verifier
-        if size >= MAX_RULES {
-            return false;
-        }
-        let mut left = 0;
-        let mut right = size;
-        #[cfg(not(feature = "user"))]
-        let mut i = 0;
-        while left < right {
-            let mid = left + size / 2;
-
-            // This can never happen but we need the verifier to believe us
-            let r = if mid < MAX_RULES {
-                // SAFETY: We are already bound checking
-                *unsafe { self.rules.get_unchecked(mid) }
-            } else {
-                return false;
-            };
-            let cmp = start(r) <= val;
-            if cmp {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-            size = right - left;
-            #[cfg(not(feature = "user"))]
-            {
-                i += 1;
-                // This should never happen, here just to satisfy verifier
-                if i >= MAX_ITER {
-                    return false;
-                }
-            }
-        }
-
-        if left == 0 {
-            false
-        } else {
-            let indx = left - 1;
-            if indx >= MAX_RULES {
-                return false;
-            }
-            // SAFETY: Again, we are already bound checking
-            end(*unsafe { self.rules.get_unchecked(indx) }) >= val
-        }
-    }
 }
 
 #[cfg(feature = "user")]
---
+++ userspace/firewall-common/src/rule_store/lookup.rs
@@ -0,0 +1,74 @@
+use super::{end, start, RuleStore, MAX_RULES};
+
+#[cfg(not(feature = "user"))]
+const MAX_ITER: u32 = MAX_RULES.ilog2() + 1;
+
+impl RuleStore {
+    // TODO: We need to check if this works in older kernels.
+    // If it doesn't we need to use --unroll-loop.
+    // We might need to refactor the loop to explicitly use `MAX_ITER`
+    // Note: MAX_ITER is at most 16
+    pub fn lookup(&self, val: u16) -> bool {
+        // 0 means all ports
+        if self.rules_len > 0 {
+            // SAFETY: We know that rules_len < MAX_RULES
+            if start(*unsafe { self.rules.get_unchecked(0) }) == 0 {
+                return true;
+            }
+        }
+
+        // We test for 0 in all non tcp/udp packets
+        // it's worth returning early for those cases.
+        if val == 0 {
+            return false;
+        }
+
+        // Reimplementation of partition_point to satisfy verifier
+        let mut size = self.rules_len as usize;
+        // appeasing the verifier
+        if size >= MAX_RULES {
+            return false;
+        }
+        let mut left = 0;
+        let mut right = size;
+        #[cfg(not(feature = "user"))]
+        let mut i = 0;
+        while left < right {
+            let mid = left + size / 2;
+
+            // This can never happen but we need the verifier to believe us
+            let r = if mid < MAX_RULES {
+                // SAFETY: We are already bound checking
+                *unsafe { self.rules.get_unchecked(mid) }
+            } else {
+                return false;
+            };
+            let cmp = start(r) <= val;
+            if cmp {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+            size = right - left;
+            #[cfg(not(feature = "user"))]
+            {
+                i += 1;
+                // This should never happen, here just to satisfy verifier
+                if i >= MAX_ITER {
+                    return false;
+                }
+            }
+        }
+
+        if left == 0 {
+            false
+        } else {
+            let indx = left - 1;
+            if indx >= MAX_RULES {
+                return false;
+            }
+            // SAFETY: Again, we are already bound checking
+            end(*unsafe { self.rules.get_unchecked(indx) }) >= val
+        }
+    }
+}
